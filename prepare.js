9eccf1d0aabe87633ed6e86939a367e047c4a86b
gzdaijie <gzdaijie@163.com>
2019-07-15 01:51:57 +0800

    forbid candan

--- prepare.js
+++ prepare.js
@@ -7,6 +7,9 @@ const SOURCE_POST_DIR = 'source/_posts/';
 
 const walkDir = (dir, callback) => {
     fs.readdirSync(dir).forEach(f => {
+        if (f.startsWith('.')) {
+            return;
+        }
         let dirPath = path.join(dir, f);
         let isDirectory = fs.statSync(dirPath).isDirectory();
         isDirectory ?
@@ -18,7 +21,7 @@ exec(['rm -rf', SOURCE_POST_DIR].join(' '))
 exec(['mkdir -p', SOURCE_POST_DIR].join(' '))
 
 walkDir('./posts', (filePath) => {
-    if (filePath.endsWith('.md') && !filePath.endsWith('posts/README.md')) {
+    if (filePath.endsWith('.md') && !filePath.endsWith('README.md')) {
         let assetsDirPath = filePath.slice(0, -3);
         exec(['cp -p', filePath, SOURCE_POST_DIR].join(' '))
         if (fs.existsSync(assetsDirPath)) {
