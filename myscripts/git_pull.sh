7e0ca22675173f4a0f0fd9020180912a6e688b9a
gzdaijie <gzdaijie@gmail.com>
2020-03-07 16:04:33 +0800

    add git pull manually to replace submodule

---
+++ myscripts/git_pull.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eou pipefail
+
+root=$(cd $(dirname $0)/..; pwd)
+
+pull() {
+    repo="$1"
+    dest="$2"
+    echo "update $dest"
+    if [ -d "$dest" ]; then
+        git -C "$dest" pull
+    else
+        git clone "https://github.com/$repo" "$dest"
+    fi;
+}
+
+cd $root
+pull geektutu/hexo-theme-geektutu themes/geektutu
+pull geektutu/7days-golang posts/7days-golang
+pull geektutu/interview-questions posts/interview-questions
+pull geektutu/tensorflow2-docs-zh posts/tensorflow2-docs-zh
--- myscripts/install_qiniu.sh
+++ myscripts/install_qiniu.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 set -eou pipefail
 
 root=$(cd $(dirname $0)/..; pwd)
 
-echo "down qiniu"
+echo "download qiniu"
 cd $root/myscripts
 rm -f qshell*
 wget http://devtools.qiniu.com/qshell-linux-x86-v2.4.1.zip
@@ -12,13 +11,3 @@ unzip qshell-linux-x86-v2.4.1.zip
 mv ./qshell-linux-x86-v2.4.1 qshell
 export PATH=$root/myscripts:$PATH
 ./qshell account ${QQ_AccessKey} ${QQ_SecretKey} ${QQ_Name}
-
-echo "github comment"
-yarn comment
-
-cd $root
-echo "hexo build"
-yarn build
-
-echo "upload qiniu"
-yarn qiniu
