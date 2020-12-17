28e1b9d0af6d177d80a40777c1cb01f9486a4af0
gzdaijie <gzdaijie@gmail.com>
2020-12-18 00:56:22 +0800

    use github actions to replace travis

--- myscripts/qiniu_upload.sh
+++ myscripts/qiniu_upload.sh
@@ -10,7 +10,7 @@ wget http://devtools.qiniu.com/qshell-linux-x86-v2.4.1.zip
 unzip qshell-linux-x86-v2.4.1.zip
 mv ./qshell-linux-x86-v2.4.1 qshell
 export PATH=$root/myscripts:$PATH
-./qshell account ${QQ_AccessKey} ${QQ_SecretKey} ${QQ_Name}
+./qshell account ${QQ_ACCESSKEY} ${QQ_SECRETKEY} ${QQ_NAME}
 
 # upload
 yarn qiniu
